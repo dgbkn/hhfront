725a9941192c5471731e66c79d167e5b97310c1b
_dev_goyal__ <pwwalabkn@gmail.com>
2023-04-16 10:59:43 +0530

    dev

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -202,7 +202,7 @@ export default function Login() {
 
 
   return (
-    <>
+    <div>
       <Modal isOpen={isModalOpen} onClose={() => { setisModalOpen(false) }} >
         <ModalOverlay />
         <ModalContent>
@@ -280,7 +280,6 @@ export default function Login() {
                 ))}
               </AvatarGroup>
               <Text fontFamily={'heading'} fontSize={{ base: '4xl', md: '6xl' }}>
-                +
               </Text>
               <Flex
                 align={'center'}
@@ -360,34 +359,34 @@ export default function Login() {
             form
           </Stack>
         </Container>
-        {/* <Blur
+        <Blur
           position={'absolute'}
           top={-5}
           left={-5}
           style={{ filter: 'blur(80px)' }}
-        /> */}
+        />
       </Box>
-    </>
+    </div>
   );
 }
 
-// export const Blur = (props) => {
-//   return (
-//     <Icon
-//       width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
-//       zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
-//       height="200px"
-//       viewBox="0 -450 528 560"
-//       fill="none"
-//       xmlns="http://www.w3.org/2000/svg"
-//       {...props}>
-//       <circle cx="71" cy="61" r="111" fill="#F56565" />
-//       <circle cx="244" cy="106" r="139" fill="#ED64A6" />
-//       <circle cy="291" r="139" fill="#ED64A6" />
-//       <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
-//       <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
-//       <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
-//       <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
-//     </Icon>
-//   );
-// };
+export const Blur = (props) => {
+  return (
+    <Icon
+      width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
+      zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
+      height="200px"
+      viewBox="0 -450 528 560"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <circle cx="71" cy="61" r="111" fill="#F56565" />
+      <circle cx="244" cy="106" r="139" fill="#ED64A6" />
+      <circle cy="291" r="139" fill="#ED64A6" />
+      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
+      <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
+      <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
+      <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+    </Icon>
+  );
+};
